c160a84dc241bf7988405ff55d6518e84cca8df6
DaedalusMinor <34559192+DaedalusMinor@users.noreply.github.com>
2019-11-20 23:31:19 -0500

    Add files via upload

--- Fractal/fractalGen.js
+++ Fractal/fractalGen.js
@@ -1,24 +1,23 @@
-var x = window.innerWidth/2;
-var y = window.innerHeight/2 - 1;
-var lengthSlider = document.getElementById("lengthSlider");
-var branchSlider = document.getElementById("branchSlider");
-var lengthValue = document.getElementById("lengthValue");
-var branchValue = document.getElementById("branchValue");
-var partitionSlider = document.getElementById("partitionSlider");
-var partitionValue = document.getElementById("partitionValue");
+let x = canvas.width/2;
+let y = canvas.height/2 - 1;
+let lengthSlider = document.getElementById("lengthSlider");
+let branchSlider = document.getElementById("branchSlider");
+let lengthValue = document.getElementById("lengthValue");
+let branchValue = document.getElementById("branchValue");
+let partitionSlider = document.getElementById("partitionSlider");
+let partitionValue = document.getElementById("partitionValue");
 
-var radialSliders = [];
-var radialValues = [];
+let radialSliders = [];
+let radialValues = [];
 
-for(var i = 1; i <= 6; i++){
-	var slider = "radialSlider" + i;
-	var value = "radialValue" + i;
+for(let i = 1; i <= 6; i++) {
+	let slider = "radialSlider" + i;
+	let value = "radialValue" + i;
 	radialSliders[i-1] = document.getElementById(slider);
 	radialValues[i-1] = document.getElementById(value);
 }
 
-window.onload = function()
-{
+window.onload = function() {
 	canvas=document.getElementById("canvas");
 	ctx=canvas.getContext("2d");
 	canvas.width=window.innerWidth;
@@ -28,7 +27,7 @@ window.onload = function()
 }
 
 Mouse = function() {
-	var mouse = {};
+	let mouse = {};
 	mouse.x = 0;
 	mouse.y = 0;
 
@@ -43,25 +42,25 @@ Mouse = function() {
 
 function main(){
 	ctx.fillStyle = 'black';
-	ctx.fillRect(0,0,window.innerWidth, window.innerHeight);
+	ctx.fillRect(0,0,canvas.width, canvas.height);
 	ctx.beginPath();
 	ctx.strokeStyle = 'white';
 
-	var y = mouse.y - window.innerHeight/2;
-	var x = mouse.x - window.innerWidth/2;
-	var branches = branchSlider.value;
-	var length = lengthSlider.value;
-	var partitions = partitionSlider.value;
+	let y = mouse.y - canvas.height/2;
+	let x = mouse.x - canvas.width/2;
+	let branches = branchSlider.value;
+	let length = lengthSlider.value;
+	let partitions = partitionSlider.value;
 	partitionValue.innerHTML = partitions;
 	lengthValue.innerHTML = length;
 	branchValue.innerHTML = branches;
 
-	for(var i = 0; i < radialValues.length; i++){
-		if(i < partitions){
+	for (let i = 0; i < radialValues.length; i++) {
+		if (i < partitions) {
 			radialSliders[i].disabled = false;
 			radialValues[i].innerHTML = radialSliders[i].value;
 		}
-		else{
+		else {
 			radialValues[i].innerHTML = "";
 			radialSliders[i].value = 0;
 			radialSliders[i].disabled = true;
@@ -69,21 +68,20 @@ function main(){
 		}
 	}
 	theta = Math.atan2(y, x);
-	drawTree(branches, theta, length, window.innerWidth/2, window.innerHeight/2);
+	drawTree(branches, theta, length, canvas.width/2, canvas.height/2);
 	ctx.stroke();
 }
 
-function drawTree(stems, theta, longness, startX, startY){
-	if(stems >= 1){
+function drawTree(stems, theta, longness, startX, startY) {
+	if(stems >= 1) {
 		ctx.moveTo(startX, startY);
 		ctx.lineWidth = stems;
 		ctx.lineTo(startX + longness * Math.cos(theta), startY + longness*Math.sin(theta));
 
-		for(var i = 0; i < radialSliders.length; i++){
-			if(!radialSliders[i].disabled){
+		for (let i = 0; i < radialSliders.length; i++) {
+			if(!radialSliders[i].disabled) {
 				drawTree(stems-1, theta * radialSliders[i].value, longness/1.618, startX + longness * Math.cos(theta), startY + longness * Math.sin(theta));
 			}
 		}
-
 	}
 }
